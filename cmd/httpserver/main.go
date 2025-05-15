c983c86a9b4a5a3a72b875800e7d4faa8530e3ec
John <128350391+alpaOo@users.noreply.github.com>
2025-05-15 20:38:36 +0800

    feat: Add custom handler in main

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/johndosdos/http-from-tcp/internal/request"
+	"github.com/johndosdos/http-from-tcp/internal/response"
 	"github.com/johndosdos/http-from-tcp/internal/server"
 )
 
 func main() {
 	const port = 42069
 
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, handlerRequest)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
@@ -23,4 +26,30 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+func handlerRequest(w io.Writer, req *request.Request) *server.HandlerError {
+	if req.RequestLine.RequestTarget == "/yourproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    "Your problem is not my problem\n",
+		}
+	}
+
+	if req.RequestLine.RequestTarget == "/myproblem" {
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			Message:    "Woopsie, my bad\n",
+		}
+	}
+
+	_, err := io.WriteString(w, "All good, frfr\n")
+	if err != nil {
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			Message:    "an error occurred while writing the response.\n",
+		}
+	}
+
+	return nil
 }
